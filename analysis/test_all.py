e9d3802d0b07a302aa9c7e66bd625abd1a4345d0
colinvwood <woodcolin38@gmail.com>
2023-01-19 15:33:57 -0700

    tests for tree class

--- analysis/test_all.py
+++ analysis/test_all.py
@@ -79,10 +79,12 @@ class TestGenome:
 
 
 class TestTree:
+  tree = Tree()
+
   @pytest.fixture
   def source_pop(self):
     genomes = [
-      Genome([1,2,3,4,8]),
+      Genome([1,2,3,4,8]), # clone
       Genome([1,2,3,4,5]),
       Genome([1,3]),
     ]
@@ -91,7 +93,7 @@ class TestTree:
   @pytest.fixture
   def recipient_pop(self):
     genomes = [
-      Genome([1,2,3,7]),
+      Genome([1,2,3,7]), # clone
       Genome([1,2,3,4,5]),
       Genome([1,2,4]),
     ]
@@ -99,28 +101,113 @@ class TestTree:
   
   @pytest.fixture
   def branches(self, source_pop, recipient_pop):
-    tree = Tree()
     src_genome = source_pop.population[0]
     rec_genome = recipient_pop.population[0]
-    branches = tree.categorize_mutations(src_genome, rec_genome)
+    branches = self.tree.categorize_mutations(src_genome, rec_genome)
     return branches
-
+  
+  @pytest.fixture
+  def proportion_branches(self, branches, source_pop, recipient_pop):
+    return self.tree.assign_proportions(branches, source_pop, recipient_pop) 
+     
   def test_categorize_mutations(self, branches):
     shared_branch, source_branch, recipient_branch = branches.values()
     assert list(shared_branch.keys()) == [1,2,3]
     assert list(source_branch.keys()) == [4,8]
     assert list(recipient_branch.keys()) == [7]
 
-  def test_assign_proportions(self, branches, source_pop, recipient_pop):
-    tree = Tree()
-    branches_proportions = tree.assign_proportions(branches, source_pop, 
-                                                   recipient_pop) 
-
+  def test_assign_proportions(self, proportion_branches):
     shared_proportions, source_proportions, recipient_proportions = \
-      branches_proportions.values()
+      proportion_branches.values()
     
     assert list(shared_proportions.keys()) == [1,2,3]
     assert list(source_proportions.keys()) == [4,8]
     assert list(recipient_proportions.keys()) == [7]
+    assert shared_proportions[1]["source_proportion"] == 1
     
+  def test_count_segregating_snps(self):
+    src_genomes = [
+      Genome([1,2,3,4,5,7]), # clone
+      Genome([1,2,4,5]),
+      Genome([1,2,3,8]),
+    ]
+    source_pop = Population.from_genomes(src_genomes, 10)
+  
+    rec_genomes = [
+      Genome([1,2,3,4,6,9]), # clone
+      Genome([1,2,3,4,5]),
+      Genome([1,2,4]),
+    ]
+    recipient_pop = Population.from_genomes(rec_genomes, 10)
 
+    tree = Tree.initialized(src_genomes[0], rec_genomes[0], source_pop, 
+                            recipient_pop)
+
+    shared_src_segs = tree.count_segregating_snps("source", tree.shared_branch)
+    assert shared_src_segs == 2 # [3,4]
+    shared_rec_segs = tree.count_segregating_snps("recipient", 
+                                                  tree.shared_branch)
+    assert shared_rec_segs == 1 # [3]
+
+  def test_check_tier_1(self):
+    shared_branch = {
+      1: {
+        "source_proportion": 0.5,
+        "recipient_proportion": 1, 
+      },
+      2: {
+        "source_proportion": 1,
+        "recipient_proportion": 1, 
+      },
+      3: {
+        "source_proportion": 0.3,
+        "recipient_proportion": 0.9, 
+      },
+    }
+    assert self.tree.check_tier_1(shared_branch) == 1
+
+    shared_branch[1]["source_proportion"] = 1
+    assert self.tree.check_tier_1(shared_branch) == 0
+
+    shared_branch = {}
+    assert self.tree.check_tier_1(shared_branch) == 0
+  
+  def test_check_tier_2(self):
+    source_branch = {
+      1: {
+        "source_proportion": 0.5,
+        "recipient_proportion": 1, 
+      },
+      2: {
+        "source_proportion": 1,
+        "recipient_proportion": 1, 
+      },
+      3: {
+        "source_proportion": 0.8,
+        "recipient_proportion": 1, 
+      },
+    }
+    recipient_branch = {
+      1: {
+        "source_proportion": 0.5,
+        "recipient_proportion": 1, 
+      },
+      2: {
+        "source_proportion": 1,
+        "recipient_proportion": 1, 
+      },
+      3: {
+        "source_proportion": 1,
+        "recipient_proportion": 0.9, 
+      },
+    }
+    assert self.tree.check_tier_2(source_branch, recipient_branch) == 1
+
+    recipient_branch[1]["source_proportion"] = 1
+    assert self.tree.check_tier_2(source_branch, recipient_branch) == 0
+
+    recipient_branch[1]["source_proportion"] = 0.5
+    source_branch[1]["recipient_proportion"] = 0.9
+    assert self.tree.check_tier_2(source_branch, recipient_branch) == 0
+
+    assert self.tree.check_tier_2({}, {}) == 0
