75d36e783ee1c3d4cec66ebf318e10568353a821
colinvwood <woodcolin38@gmail.com>
2023-01-29 23:10:40 -0700

    method interface changes

--- analysis/tests/test_population.py
+++ analysis/tests/test_population.py
@@ -5,8 +5,6 @@ from Genome import Genome
 
 
 class TestPopulation:
-  pop = Population()
-
   def test_parse_csv(self):
     with open("test_file.csv", 'w') as file:
       file.write("200,12345,2700000,3,0\n")
@@ -14,12 +12,11 @@ class TestPopulation:
       file.write("\n")
       file.write("7,\n")
 
-    self.pop.population = self.pop.parse_csv("test_file.csv")
-
-    assert len(self.pop.population) == 4
-    assert len(self.pop.population[0].mutations) == 5
-    assert len(self.pop.population[2].mutations) == 0
-    assert self.pop.population[1].mutations == [4600, 300500, 13]
+    pop = Population.from_csv_file("test_file.csv", sample_size=0)
+    assert len(pop.population) == 4
+    assert len(pop.population[0].mutations) == 5
+    assert len(pop.population[2].mutations) == 0
+    assert pop.population[1].mutations == [4600, 300500, 13]
   
   def test_population_from_genomes(self):
     genomes = [
@@ -28,29 +25,30 @@ class TestPopulation:
       Genome([1,3,4,5,7]),
       Genome([10]),
     ]
-    pop = Population.from_genomes(genomes, sample_size = 0)
-
+    pop = Population.from_genomes(genomes, sample_size=0)
     assert len(pop.population) == 4 
     assert pop.population[2].mutations == [1,3,4,5,7]
     assert pop.population[1].mutations == []
 
   def test_sample_population(self):
-    pop = Population.from_csv_file("test_file.csv", sample_size = 10)
-    
+    pop = Population.from_csv_file("test_file.csv", sample_size=10)
     assert len(pop.sample) == 10
     assert isinstance(pop.sample[0], Genome)
-    assert len(pop.sample_population(pop.sample, 0)) == 0
+
+    pop = Population.from_csv_file("test_file.csv", sample_size=0)
+    assert len(pop.sample) == 0
      
   def test_get_sample_snps(self):
+    pop = Population()
     sample = [
       Genome([1,2,3,4,5]),
       Genome([1,2,3,4]),
       Genome([1,3,4,6]),
       Genome([1,8]),
     ]
-
-    snps = self.pop.get_sample_snps(sample)
-    
+    pop.sample = sample
+    pop.get_sample_snps()
+    snps = pop.sample_snps
     assert len(snps) == 7
     assert 7 not in snps
     assert snps[1]["count"] == 4
@@ -58,12 +56,11 @@ class TestPopulation:
     assert snps[3]["proportion"] == 0.75 
     assert snps[8]["proportion"] == 0.25
     
-    print(snps)
-
   def test_count_unique_genomes(self):
-    assert self.pop.count_unique_genomes([]) == 0
-    assert self.pop.count_unique_genomes([Genome([])]) == 1
-    assert self.pop.count_unique_genomes([Genome([]), Genome([])]) == 1
+    pop = Population()
+    assert pop.count_unique_genomes([]) == 0
+    assert pop.count_unique_genomes([Genome([])]) == 1
+    assert pop.count_unique_genomes([Genome([]), Genome([])]) == 1
 
     population = [
       Genome([1,2,3,4]),
@@ -71,7 +68,7 @@ class TestPopulation:
       Genome([1,3,4,6]),
       Genome([1,8]),
     ]
-    assert self.pop.count_unique_genomes(population) == 3
+    assert pop.count_unique_genomes(population) == 3
 
   def teardown_class(cls):
     import os
--- analysis/tests/test_tree.py
+++ analysis/tests/test_tree.py
@@ -7,8 +7,6 @@ from Population import Population
 from Genome import Genome
 
 class TestTree:
-  tree = Tree()
-
   @pytest.fixture
   def source_pop(self):
     genomes = [
@@ -16,7 +14,10 @@ class TestTree:
       Genome([1,2,3,4,5]),
       Genome([1,3]),
     ]
-    return Population.from_genomes(genomes, 10)
+    pop = Population()
+    pop.sample = genomes
+    pop.get_sample_snps()
+    return pop
 
   @pytest.fixture
   def recipient_pop(self):
@@ -25,60 +26,81 @@ class TestTree:
       Genome([1,2,3,4,5]),
       Genome([1,2,4]),
     ]
-    return Population.from_genomes(genomes, 10)
-  
-  @pytest.fixture
-  def branches(self, source_pop, recipient_pop):
-    src_genome = source_pop.population[0]
-    rec_genome = recipient_pop.population[0]
-    branches = self.tree.categorize_mutations(src_genome, rec_genome)
-    return branches
-  
-  @pytest.fixture
-  def proportion_branches(self, branches, source_pop, recipient_pop):
-    return self.tree.assign_proportions(branches, source_pop, recipient_pop) 
-     
-  def test_categorize_mutations(self, branches):
-    shared_branch, source_branch, recipient_branch = branches.values()
-    assert list(shared_branch.keys()) == [1,2,3]
-    assert list(source_branch.keys()) == [4,8]
-    assert list(recipient_branch.keys()) == [7]
+    pop = Population()
+    pop.sample = genomes
+    pop.get_sample_snps()
+    return pop
 
-  def test_assign_proportions(self, proportion_branches):
-    shared_proportions, source_proportions, recipient_proportions = \
-      proportion_branches.values()
-    
-    assert list(shared_proportions.keys()) == [1,2,3]
-    assert list(source_proportions.keys()) == [4,8]
-    assert list(recipient_proportions.keys()) == [7]
-    assert shared_proportions[1]["source_proportion"] == 1
+  @pytest.fixture
+  def tree(self, source_pop, recipient_pop):
+    return Tree.initialized(source_pop.sample[0],
+                            recipient_pop.sample[0],
+                            source_pop,
+                            recipient_pop)
+
+  def test_categorize_mutations(self, tree):
+    assert list(tree.shared_branch.keys()) == [1,2,3]
+    assert list(tree.source_branch.keys()) == [4,8]
+    assert list(tree.recipient_branch.keys()) == [7]
+
+  def test_assign_proportions(self, tree):
+    assert tree.shared_branch[1]["source_proportion"] == 1
+    assert tree.shared_branch[1]["recipient_proportion"] == 1
+    assert tree.shared_branch[2]["source_proportion"] == 2/3
+    assert tree.shared_branch[3]["recipient_proportion"] == 2/3
+    assert 5 not in tree.shared_branch
+    assert tree.source_branch[8]["source_proportion"] == 1/3
+    assert tree.source_branch[8]["recipient_proportion"] == 0
+    assert tree.recipient_branch[7]["recipient_proportion"] == 1/3
+    assert tree.recipient_branch[7]["source_proportion"] == 0
     
   def test_count_segregating_snps(self):
+    branch = {
+      1: {
+        "source_proportion": 0.6,
+        "recipient_proportion": 1,
+      },
+      2: {
+        "source_proportion": 0.9,
+        "recipient_proportion": 1,
+      },
+      3: {
+        "source_proportion": 0,
+        "recipient_proportion": 0,
+      },
+      4: {
+        "source_proportion": 1,
+        "recipient_proportion": 0,
+      },
+    }
+    tree = Tree()
+    src_segs = tree.count_segregating_snps("source", branch)
+    rec_segs = tree.count_segregating_snps("recipient", branch)
+    assert src_segs == 2
+    assert rec_segs == 0
+
     src_genomes = [
       Genome([1,2,3,4,5,7]), # clone
       Genome([1,2,4,5]),
       Genome([1,2,3,8]),
     ]
-    source_pop = Population.from_genomes(src_genomes, 10)
-  
+    source_pop = Population.from_genomes(src_genomes, 20)
     rec_genomes = [
       Genome([1,2,3,4,6,9]), # clone
       Genome([1,2,3,4,5]),
       Genome([1,2,4]),
     ]
-    recipient_pop = Population.from_genomes(rec_genomes, 10)
-
+    recipient_pop = Population.from_genomes(rec_genomes, 20)
     tree = Tree.initialized(src_genomes[0], rec_genomes[0], source_pop, 
                             recipient_pop)
-
     shared_src_segs = tree.count_segregating_snps("source", tree.shared_branch)
-    assert shared_src_segs == 2 # [3,4]
+    assert shared_src_segs == 2, "may fail due to random sampling" # [3,4]
     shared_rec_segs = tree.count_segregating_snps("recipient", 
                                                   tree.shared_branch)
-    assert shared_rec_segs == 1 # [3]
+    assert shared_rec_segs == 1, "may fail due to random sampling" # [3]
 
   def test_check_tier_1(self):
-    shared_branch = {
+    branch = {
       1: {
         "source_proportion": 0.5,
         "recipient_proportion": 1, 
@@ -92,16 +114,21 @@ class TestTree:
         "recipient_proportion": 0.9, 
       },
     }
-    assert self.tree.check_tier_1(shared_branch) == 1
+    tree = Tree()
+    tree.shared_branch = branch
+    assert tree.check_tier_1() == 1
 
-    shared_branch[1]["source_proportion"] = 1
-    assert self.tree.check_tier_1(shared_branch) == 0
+    tree.shared_branch[1]["source_proportion"] = 1
+    assert tree.check_tier_1() == 0
 
-    shared_branch = {}
-    assert self.tree.check_tier_1(shared_branch) == 0
+    tree.shared_branch = {}
+    assert tree.check_tier_1() == 0
   
   def test_check_tier_2(self):
-    assert self.tree.check_tier_2(source_branch={}, recipient_branch={}) == 0
+    tree = Tree()
+    tree.source_branch = {}
+    tree.recipient_branch = {}
+    assert tree.check_tier_2() == 0
 
     source_branch = {
       1: {
@@ -131,52 +158,56 @@ class TestTree:
         "recipient_proportion": 0.9, 
       },
     }
-    assert self.tree.check_tier_2(source_branch, recipient_branch) == 1
+    tree.source_branch = source_branch
+    tree.recipient_branch = recipient_branch
+    assert tree.check_tier_2() == 1
 
-    recipient_branch[1]["source_proportion"] = 1
-    assert self.tree.check_tier_2(source_branch, recipient_branch) == 0
+    tree.recipient_branch[1]["source_proportion"] = 1
+    assert tree.check_tier_2() == 0
 
-    recipient_branch[1]["source_proportion"] = 0.5
-    source_branch[1]["recipient_proportion"] = 0.9
-    assert self.tree.check_tier_2(source_branch, recipient_branch) == 0
-
+    tree.recipient_branch[1]["source_proportion"] = 0.5
+    tree.source_branch[1]["recipient_proportion"] = 0.9
+    assert tree.check_tier_2() == 0
 
   def test_bin_proportions(self):
-    assert self.tree.bin_proportions([], num_bins=1) == []
-    assert self.tree.bin_proportions([1], num_bins=1) == [1]
-    assert self.tree.bin_proportions([1,1], num_bins=2) == [0,2]
-    assert self.tree.bin_proportions([1,1], num_bins=1) == [2]
-    assert self.tree.bin_proportions([1, 0.1, 1], num_bins=2) == [1, 2]
+    tree = Tree()
+    assert tree.bin_proportions([], num_bins=1) == []
+    assert tree.bin_proportions([1], num_bins=1) == [1]
+    assert tree.bin_proportions([1,1], num_bins=2) == [0,2]
+    assert tree.bin_proportions([1,1], num_bins=1) == [2]
+    assert tree.bin_proportions([1, 0.1, 1], num_bins=2) == [1, 2]
 
     proportions = [1, 0.4, 0.8, 1]
-    binned_proportions = self.tree.bin_proportions(proportions, num_bins=10)
+    binned_proportions = tree.bin_proportions(proportions, num_bins=10)
     assert binned_proportions == [0, 0, 0, 1, 0, 0, 0, 1, 0, 2]
 
   def test_psuedo_entropy(self):
-    assert self.tree.psuedo_entropy([]) == 0
-    assert self.tree.psuedo_entropy([1]) == 0
-    assert self.tree.psuedo_entropy([30]) == 0
-    assert self.tree.psuedo_entropy([0, 1]) == 0
-    assert self.tree.psuedo_entropy([0, 99]) == 0
-    assert self.tree.psuedo_entropy([1, 1]) == 1
+    tree = Tree()
+    assert tree.psuedo_entropy([]) == 0
+    assert tree.psuedo_entropy([1]) == 0
+    assert tree.psuedo_entropy([30]) == 0
+    assert tree.psuedo_entropy([0, 1]) == 0
+    assert tree.psuedo_entropy([0, 99]) == 0
+    assert tree.psuedo_entropy([1, 1]) == 1
 
-    source_props_binned = [0, 0, 1, 3, 3]
-    recipient_props_binned = [0, 0, 0, 0, 7]
-    source_entropy = self.tree.psuedo_entropy(source_props_binned)
-    recipient_entropy = self.tree.psuedo_entropy(recipient_props_binned)
+    source_proportions_binned = [0, 0, 1, 3, 3]
+    recipient_proportions_binned = [0, 0, 0, 0, 7]
+    source_entropy = tree.psuedo_entropy(source_proportions_binned)
+    recipient_entropy = tree.psuedo_entropy(recipient_proportions_binned)
     assert source_entropy > recipient_entropy
 
-    source_entropy = self.tree.psuedo_entropy([1, 1, 0, 0, 0])
-    recipient_entropy = self.tree.psuedo_entropy([1, 0, 0, 0, 1])
+    source_entropy = tree.psuedo_entropy([1, 1, 0, 0, 0])
+    recipient_entropy = tree.psuedo_entropy([1, 0, 0, 0, 1])
     assert source_entropy == recipient_entropy
 
     # important to understand the phylogenetic implications of this one 
-    source_entropy = self.tree.psuedo_entropy([3, 1, 0, 0])
-    recipient_entropy = self.tree.psuedo_entropy([2, 2, 0, 0])
+    source_entropy = tree.psuedo_entropy([3, 1, 0, 0])
+    recipient_entropy = tree.psuedo_entropy([2, 2, 0, 0])
     assert source_entropy < recipient_entropy
   
   def test_compare_clumpiness(self):
-    assert self.tree.compare_clumpiness({}, num_bins=10) == 0
+    tree = Tree()
+    assert tree.compare_clumpiness({}, num_bins=10) == 0
 
     branch = {
       1: {
@@ -192,9 +223,9 @@ class TestTree:
         "recipient_proportion": 0.9, 
       },
     }
-    assert self.tree.compare_clumpiness(branch, num_bins=10) == 1
-    assert self.tree.compare_clumpiness(branch, num_bins=4) == 1
-    assert self.tree.compare_clumpiness(branch, num_bins=3) == 0
+    assert tree.compare_clumpiness(branch, num_bins=10) == 1
+    assert tree.compare_clumpiness(branch, num_bins=4) == 1
+    assert tree.compare_clumpiness(branch, num_bins=3) == 0
 
     branch = {
       1: {
@@ -206,7 +237,7 @@ class TestTree:
         "recipient_proportion": 1,
       },
     }
-    assert self.tree.compare_clumpiness(branch, num_bins=10) == 0
+    assert tree.compare_clumpiness(branch, num_bins=10) == 0
 
     branch[2]["recipient_proportion"] == 0.8
-    assert self.tree.compare_clumpiness(branch, num_bins=10) == 0
+    assert tree.compare_clumpiness(branch, num_bins=10) == 0
