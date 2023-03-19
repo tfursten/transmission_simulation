a060f4a893e77db5e8058df33098a8e367121153
colinvwood <woodcolin38@gmail.com>
2023-03-19 10:57:22 -0700

    results restructuring, add ambiguous detection method

--- analysis/Analysis.py
+++ analysis/Analysis.py
@@ -11,54 +11,7 @@ class Analysis:
     self.recipient_pop = None
     self.sample_size = None
     self.num_clumpiness_bins = None
-    self.results = {
-      "tier 1": {
-        "values": {
-          "correct detection": [],
-          "reverse detection": []
-        },
-        "proportion": {
-          "correct detection": None,
-          "reverse detection": None
-        }
-      },
-      "tier 2": {
-        "values": {
-          "correct detection": [],
-          "reverse detection": []
-        },
-        "proportion": {
-          "correct detection": None,
-          "reverse detection": None
-        }
-      },
-      "clumpiness": {
-        "values": {
-          "correct detection": [],
-          "reverse detection": []
-        },
-        "proportion": {
-          "correct detection": None,
-          "reverse detection": None
-        }
-      },
-      "combined": {
-        "values": {
-          "correct detection": [],
-          "reverse detection": []
-        },
-        "proportion": {
-          "correct detection": None,
-          "reverse detection": None
-        }
-      },
-      "population counts": {
-        "source population": 0,
-        "source sample": 0,
-        "recipient population": 0,
-        "recipient sample": 0
-      }
-    }
+    self.results = {}
 
   @classmethod
   def from_params(cls, analysis_params):
@@ -82,22 +35,66 @@ class Analysis:
         self.collect_tree_results(tree)
 
   def collect_tree_results(self, tree):
-    self.results["combined"]["values"]["correct detection"].append(0)
-    self.results["combined"]["values"]["reverse detection"].append(0)
-    for statistic, function in [
-        ("tier 1", tree.check_tier_1),
-        ("tier 2", tree.check_tier_2), 
-        ("clumpiness", tree.compare_clumpiness) 
-    ]:
-      if statistic == "clumpiness":
-          output = function(tree.shared_branch, self.num_clumpiness_bins)
-      else:
-        output = function()
-        
-      for type in ("correct detection", "reverse detection"):
-        self.results[statistic]["values"][type].append(output[type]) 
-        if output[type] == 1:
-          self.results["combined"]["values"][type][-1] = 1
+    self.collect_tree_result(tree, "tier 1", tree.check_tier_1)
+    self.collect_tree_result(tree, "tier 2", tree.check_tier_2)
+    self.collect_tree_result(tree, 
+                            "clumpiness ancestral", 
+                            tree.check_clumpiness_ancestral)
+    self.collect_tree_result(tree, 
+                            "clumpiness composite", 
+                            tree.check_clumpiness_composite)
+    self.collect_tree_result(tree, "combined", self.get_combined_values)
+
+  def collect_tree_result(self, tree, statistic, check_function):
+    if statistic not in self.results:
+      self.results[statistic] = {
+        "correct detection": [],
+        "reverse detection": []
+      }
+
+    if statistic in ["clumpiness ancestral", "clumpiness composite"]:
+      result = check_function(self.num_clumpiness_bins)
+    else:
+      result = check_function()
+
+    self.results[statistic]["correct detection"].append(
+      result["correct detection"] 
+    )
+    self.results[statistic]["reverse detection"].append(
+      result["reverse detection"] 
+    )
+
+  def get_combined_values(self):
+    statistics = [key for key in self.results if key != "combined"]
+    correct_values = [
+      self.results[statistic]["correct detection"][-1] 
+      for statistic in statistics
+    ] 
+    reverse_values = [
+      self.results[statistic]["reverse detection"][-1] 
+      for statistic in statistics
+    ] 
+
+    return {
+      "correct detection": any(correct_values),
+      "reverse detection": any(reverse_values)
+    }
+  
+  def count_ambiguous_detection(self):
+    """
+    Tallies the situations in which neither a 'correct' nor a 'reverse'
+    transmission direction is called (i.e. ambiguous transmission), for each
+    statistic.
+    """
+    num_analyses = len(self.results["tier 1"]["correct detection"])
+    for statistic in self.results:
+      self.results[statistic]["ambiguous detection"] = []
+      for i in range(num_analyses):
+        if (self.results[statistic]["correct detection"][i] == 0 
+            and self.results[statistic]["reverse detection"][i] == 0):
+          self.results[statistic]["ambiguous detection"].append(1)
+        else:
+          self.results[statistic]["ambiguous detection"].append(0) 
 
   def count_populations(self):
     population_names = ("source population", "source sample", 
@@ -105,6 +102,7 @@ class Analysis:
     populations = (self.source_pop.population, self.source_pop.sample, 
                    self.recipient_pop.population, self.recipient_pop.sample)
 
+    self.results["population counts"] = {}
     for pop_name, pop in zip(population_names, populations):
       self.results["population counts"][pop_name] = \
         self.count_unique_genomes(pop)
@@ -116,26 +114,23 @@ class Analysis:
       if genome not in visited_genomes:
         visited_genomes.append(genome)
         unique_counter += 1
-    
+
     return unique_counter
 
   def get_proportions(self):
-    for statistic in ("tier 1", "tier 2", "clumpiness", "combined"):
-      self.results[statistic]["proportion"] = {
-        "correct detection": 
-          mean(self.results[statistic]["values"]["correct detection"]),
-        "reverse detection": 
-          mean(self.results[statistic]["values"]["reverse detection"])
-      }
+    proportions = {}
+    for statistic in self.results:
+      proportions[statistic] = {}
+      for direction in ("correct", "reverse", "ambiguous"):
+        proportions[statistic][f"{direction} detection proportion"] = \
+          mean(self.results[statistic][f"{direction} detection"])
+    
+    return proportions
 
   def get_output(self):
-    self.get_proportions()
-    output = {
-      "tier 1 proportion": self.results["tier 1"]["proportion"],
-      "tier 2 proportion": self.results["tier 2"]["proportion"],
-      "clumpiness proportion": self.results["clumpiness"]["proportion"],
-      "combined proportion": self.results["combined"]["proportion"],
-    }
+    self.count_ambiguous_detection()
+    output = self.get_proportions()
+
     if self.count_populations:
       self.count_populations()
       population_counts = {
