05ae13daa56a8a0474de0eb73b353208257ce1a2
Colin Wood <colinwood@brianas-iMac.local>
2023-03-19 10:46:40 -0700

    script for running multiple sim analyses

---
+++ analysis/run_many.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+# assumes run parameters file and population files in same directory
+# python run_many.py pop_files_dir id...
+if __name__ == "__main__":
+	pop_id_files_dir = sys.argv[1]
+	for id in sys.argv[2:]:
+		run_id_file = os.path.join(pop_id_files_dir, f"run_id_{id}.json")
+		command = f"python analyze.py {run_id_file} analysis_params.json " + \
+							f"{pop_id_files_dir} > run_{id}_output.json"
+		
+		os.system(command)
